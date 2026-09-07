aee1e17a23cdb2993387da648a0a5a49ac99a2f2
danilevskayaevgeniay-hub <danilevskayaevgeniay@gmail.com>
2026-09-07 11:51:27 +0300

    Update ChargedMagicWandItem.java

--- src/main/java/ru/azazel/alchemytable/item/ChargedMagicWandItem.java
+++ src/main/java/ru/azazel/alchemytable/item/ChargedMagicWandItem.java
@@ -3,10 +3,31 @@ package ru.azazel.alchemytable.item;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
+
 public class ChargedMagicWandItem extends Item {
 
     public ChargedMagicWandItem(Properties properties) {
         super(properties);
+    }
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        ItemStack stack = player.getItemInHand(hand);
+        if (!level.isClientSide) {
+            LightProjectile projectile = new LightProjectile(level, player);
+            projectile.shootFromRotation(
+                player,
+                player.getXRot(),
+                player.getYRot(),
+                0.0F,
+                1.5F,
+                1.0F
+            );
+            level.addFreshEntity(projectile);
+        }
+        return InteractionResuldHolder.sidedSucces(
+            stack,
+            level.isClientSide()
+        );
     }
 
 }
