c95095b0dcef6dd4df239ded12bf3d4d6e63b112
danilevskayaevgeniay-hub <danilevskayaevgeniay@gmail.com>
2026-09-12 11:47:36 +0300

    Update AzazelSAlchemyTableClient.java

--- src/client/java/ru/azazel/alchemytable/client/AzazelSAlchemyTableClient.java
+++ src/client/java/ru/azazel/alchemytable/client/AzazelSAlchemyTableClient.java
@@ -28,5 +28,9 @@ public class AzazelSAlchemyTableClient implements ClientModInitializer {
         ModEntities.LIGHT_PROJECTILE,
         ThrownItemRenderer::new
 );
+        EntityRendererRegistry.register(
+        ModEntities.FLAME_PROJECTILE,
+        ThrownItemRenderer::new
+);
     }
 }
