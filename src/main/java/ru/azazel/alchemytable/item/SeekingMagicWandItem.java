10616593746a86917af95731babd4dff71fe041b
danilevskayaevgeniay-hub <danilevskayaevgeniay@gmail.com>
2026-09-14 11:59:12 +0300

    Create SeekingMagicWandItem.java

---
+++ src/main/java/ru/azazel/alchemytable/item/SeekingMagicWandItem.java
@@ -0,0 +1,54 @@
+package ru.azazel.alchemytable.item;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+import java.util.List;
+
+public class SeekingMagicWandItem extends Item {
+
+    public SeekingMagicWandItem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(
+            Level level,
+            Player player,
+            InteractionHand hand
+    ) {
+
+        ItemStack stack = player.getItemInHand(hand);
+
+        if (!level.isClientSide) {
+
+            List<Mob> mobs = level.getEntitiesOfClass(
+                    Mob.class,
+                    player.getBoundingBox().inflate(20.0D),
+                    mob -> player.distanceToSqr(mob) <= 400.0D
+            );
+
+            for (Mob mob : mobs) {
+                mob.addEffect(
+                        new MobEffectInstance(
+                                MobEffects.GLOWING,
+                                600,
+                                0
+                        )
+                );
+            }
+        }
+
+        return InteractionResultHolder.sidedSuccess(
+                stack,
+                level.isClientSide()
+        );
+    }
+}
