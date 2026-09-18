6504a0ede5b7c4b54747bb7cd99a3aff5d32b180
danilevskayaevgeniay-hub <danilevskayaevgeniay@gmail.com>
2026-09-18 11:50:55 +0300

    Update RedstoneProjectile.java

--- src/main/java/ru/azazel/alchemytable/entity/RedstoneProjectile.java
+++ src/main/java/ru/azazel/alchemytable/entity/RedstoneProjectile.java
@@ -1,10 +1,6 @@
 package ru.azazel.alchemytable.entity;
 
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
@@ -14,22 +10,22 @@ import net.minecraft.world.phys.Vec3;
 
 import java.util.List;
 
-public class WindProjectile extends ThrowableItemProjectile {
+public class RedstoneProjectile extends ThrowableItemProjectile {
 
-    public WindProjectile(
-            EntityType<? extends WindProjectile> entityType,
+    public RedstoneProjectile(
+            EntityType<? extends RedstoneProjectile> entityType,
             Level level
     ) {
         super(entityType, level);
     }
 
-    public WindProjectile(Level level, LivingEntity owner) {
-        super(ModEntities.WIND_PROJECTILE, owner, level);
+    public RedstoneProjectile(Level level, LivingEntity owner) {
+        super(ModEntities.REDSTONE_PROJECTILE, owner, level);
     }
 
     @Override
     protected Item getDefaultItem() {
-        return Items.WIND_CHARGE;
+        return Items.REDSTONE;
     }
 
     @Override
@@ -38,31 +34,7 @@ public class WindProjectile extends ThrowableItemProjectile {
 
         if (!level().isClientSide) {
 
-            List<Mob> mobs = level().getEntitiesOfClass(
-                    Mob.class,
-                    getBoundingBox().inflate(4.0D)
-            );
-
-            for (Mob mob : mobs) {
-
-                Vec3 direction = mob.position()
-                        .subtract(position())
-                        .normalize();
-
-                mob.push(
-                        direction.x * 2.5D,
-                        0.8D,
-                        direction.z * 2.5D
-                );
-
-                mob.addEffect(
-                        new MobEffectInstance(
-                                MobEffects.SLOW_FALLING,
-                                100,
-                                0
-                        )
-                );
-            }
+            
 
             discard();
         }
