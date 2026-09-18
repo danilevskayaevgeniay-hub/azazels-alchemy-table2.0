0da2471a51fe6707f0497b462d672e87b290edc8
danilevskayaevgeniay-hub <danilevskayaevgeniay@gmail.com>
2026-09-18 11:34:07 +0300

    Update WindMagicWandItem.java

--- src/main/java/ru/azazel/alchemytable/item/WindMagicWandItem.java
+++ src/main/java/ru/azazel/alchemytable/item/WindMagicWandItem.java
@@ -6,7 +6,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import ru.azazel.alchemytable.entity.FlameProjectile;
+import ru.azazel.alchemytable.entity.WindProjectile;
 
 public class WindMagicWandItem extends Item {
 
