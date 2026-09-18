b7bca93e08d0be9495704d33bf5e4b685846a754
danilevskayaevgeniay-hub <danilevskayaevgeniay@gmail.com>
2026-09-18 11:54:56 +0300

    Update RedstoneProjectile.java

--- src/main/java/ru/azazel/alchemytable/entity/RedstoneProjectile.java
+++ src/main/java/ru/azazel/alchemytable/entity/RedstoneProjectile.java
@@ -7,6 +7,7 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.entity.LivingEntity;
 
 import java.util.List;
 
