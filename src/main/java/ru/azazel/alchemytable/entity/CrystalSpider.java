bf8f971fa1d29598743a40a9478589f4a3e80c84
danilevskayaevgeniay-hub <danilevskayaevgeniay@gmail.com>
2026-09-21 11:34:43 +0300

    Create CrystalSpider.java

---
+++ src/main/java/ru/azazel/alchemytable/entity/CrystalSpider.java
@@ -0,0 +1,16 @@
+package ru.azazel.alchemytable.entity;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.monster.Spider;
+import net.minecraft.world.level.Level;
+
+public class CrystalSpider extends Spider {
+
+  public CrystalSpider(
+            EntityType<? extends Spider> entityType,
+            Level level
+    ) {
+        super(entityType, level);
+    }
+
+}
