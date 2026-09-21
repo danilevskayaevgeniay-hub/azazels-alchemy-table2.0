eef74ae3739604ce2589b4c0f1391d445f835619
danilevskayaevgeniay-hub <danilevskayaevgeniay@gmail.com>
2026-09-21 11:18:54 +0300

    Update RedstoneMagicWandItem.java

--- src/main/java/ru/azazel/alchemytable/item/RedstoneMagicWandItem.java
+++ src/main/java/ru/azazel/alchemytable/item/RedstoneMagicWandItem.java
@@ -36,6 +36,8 @@ public class RedstoneMagicWandItem extends Item {
             );
 
             level.addFreshEntity(projectile);
+
+            player.getCooldowns().addCooldown(this, 20);
         }
 
         return InteractionResultHolder.sidedSuccess(
