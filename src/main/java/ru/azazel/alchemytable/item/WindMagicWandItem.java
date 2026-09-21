38c3ee99b15411070be7f46a650d889644a1ff35
danilevskayaevgeniay-hub <danilevskayaevgeniay@gmail.com>
2026-09-21 11:20:31 +0300

    Update WindMagicWandItem.java

--- src/main/java/ru/azazel/alchemytable/item/WindMagicWandItem.java
+++ src/main/java/ru/azazel/alchemytable/item/WindMagicWandItem.java
@@ -36,6 +36,8 @@ public class WindMagicWandItem extends Item {
             );
 
             level.addFreshEntity(projectile);
+
+            player.getCooldowns().addCooldown(this, 20);
         }
 
         return InteractionResultHolder.sidedSuccess(
