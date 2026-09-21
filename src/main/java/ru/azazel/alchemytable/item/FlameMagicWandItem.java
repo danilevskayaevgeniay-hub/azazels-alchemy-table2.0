a14fa2c2fdd40f9b6ae549baf57252217cd89d7f
danilevskayaevgeniay-hub <danilevskayaevgeniay@gmail.com>
2026-09-21 11:16:26 +0300

    Update FlameMagicWandItem.java

--- src/main/java/ru/azazel/alchemytable/item/FlameMagicWandItem.java
+++ src/main/java/ru/azazel/alchemytable/item/FlameMagicWandItem.java
@@ -36,6 +36,8 @@ public class FlameMagicWandItem extends Item {
             );
 
             level.addFreshEntity(projectile);
+
+            player.getCooldowns().addCooldown(this, 20);
         }
 
         return InteractionResultHolder.sidedSuccess(
